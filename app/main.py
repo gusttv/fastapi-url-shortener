6c7c8b4a465dbc7dc9de01b46927bac3a09b08f4
gusttv <guga.campos10@gmail.com>
2024-12-10 09:07:21 -0300

    docs: changed commentary

--- app/main.py
+++ app/main.py
@@ -10,7 +10,6 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency for database session
 def get_db():
     db = SessionLocal()
     try:
@@ -23,7 +22,7 @@ def generate_short_url():
     chars = string.ascii_letters + string.digits
     return ''.join(random.choices(chars, k=6))
 
-# Endpoint to create short URL
+
 @app.post("/shorten", response_model=URLResponse)
 def create_short_url(url: URLCreate, db: Session = Depends(get_db)):
     """Cria uma URL encurtada"""
@@ -35,7 +34,6 @@ def create_short_url(url: URLCreate, db: Session = Depends(get_db)):
     while db.query(URL).filter(URL.short_url == short_url).first():
         short_url = generate_short_url()
 
-    # Certifique-se de que ambos os valores são strings
     new_url = URL(original_url=str(url.original_url), short_url=str(short_url))
     db.add(new_url)
     db.commit()
@@ -43,8 +41,6 @@ def create_short_url(url: URLCreate, db: Session = Depends(get_db)):
 
     return new_url
 
-
-# Endpoint to redirect to the original URL
 @app.get("/{short_url}", response_model=URLResponse)
 def redirect_to_url(short_url: str, db: Session = Depends(get_db)):
     db_url = db.query(URL).filter(URL.short_url == short_url).first()
